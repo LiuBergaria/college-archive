07d995f3eed68d5f90427123c5bf57645715108d
Helil Bergária <liubergaria@hotmail.com>
2020-11-17 20:02:21 -0300

    Correções

--- lib2/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import './ui/listview_pessoa.dart';
-
-void main() => runApp(
-      MaterialApp(
-        title: 'Carregando dados',
-        home: ListViewPessoa(),
-      ),
-    );
